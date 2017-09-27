0b19c59dbab11edd657f992d8d23505289096761
murphy <murphy@tlmworks.org>
2017-09-27 09:25:37 -0700

    moved key bindings to seperate file

--- public/js/corecodetype.js
+++ public/js/corecodetype.js
@@ -11,7 +11,7 @@ var idx = 0;
 // This is a reference for &nbsp; in the html.
 // Needed to because a " " by &nbsp; does not
 // equal " " from keyDown spacebar.
-var ref_char = $('#spacebar_ref').text();
+var ref_space = $('#spacebar_ref').text();
 
 function getChar(idx) {
     // console.log('getChar');
@@ -21,10 +21,10 @@ function getChar(idx) {
 
 function compareChar(char, event, keyid) {
     if (event.key == char || 
-        (event.code == "Space" && char == ref_char)) { 
-        return 1;
+        (event.code == "Space" && char == ref_space)) { 
+        return 2;
     } else {
-        return -1;
+        return 0;
     }
 }
 
@@ -32,13 +32,13 @@ function updateDisplay(state) {
     var display = "";
     var last = idx - 1;
 
-    if (state === 1) { 
+    if (state === 2) { 
         display = 'correct'
-    } else if ( state === 0 ) {
+    } else if ( state === 1 ) {
         // right key wrong secondary shift key
         display = 'partial';
     }
-    else if (state === -1) {
+    else if (state === 0) {
         display = 'incorrect';
     }
     
@@ -79,72 +79,13 @@ function keyHandler(event, keyid) {
 // var myBindings = { "a": function() {console.log('this is a bindings test \
 // if it had been anything else you would have informed.');} };
 
-var myBindings = { "a": keyHandler, "b": keyHandler, "c": keyHandler, 
-"d": keyHandler, "e": keyHandler, "f": keyHandler, "g": keyHandler,
-"h": keyHandler, "i": keyHandler, "j": keyHandler, "k": keyHandler,
-"l": keyHandler, "m": keyHandler, "n": keyHandler, "o": keyHandler,
-"p": keyHandler, "q": keyHandler, "r": keyHandler, "s": keyHandler,
-"t": keyHandler, "u": keyHandler, "v": keyHandler, "w": keyHandler,
-"x": keyHandler, "y": keyHandler, "z": keyHandler, 
+// An object of key:value pairs is returned 
+// to be passed into Keymap(keyBindings).
+var keyBindings =  keyHandlerBingings();
 
+var myKeyMap = new Keymap(keyBindings);
 
-// Cap lock held down
-"A": keyHandler, "B": keyHandler, "C": keyHandler, "D": keyHandler,
-"E": keyHandler, "F": keyHandler, "G": keyHandler, "H": keyHandler,
-"I": keyHandler, "J": keyHandler, "K": keyHandler, "L": keyHandler,
-"M": keyHandler, "N": keyHandler, "O": keyHandler, "P": keyHandler,
-"Q": keyHandler, "R": keyHandler, "S": keyHandler, "T": keyHandler,
-"U": keyHandler, "V": keyHandler, "W": keyHandler, "X": keyHandler, 
-"Y": keyHandler, "Z": keyHandler, 
-
-"shift_A": keyHandler, "shift_B": keyHandler, "shift_C": keyHandler, 
-"shift_D": keyHandler, "shift_E": keyHandler, "shift_F": keyHandler,
-"shift_G": keyHandler, "shift_H": keyHandler, "shift_I": keyHandler,
-"shift_J": keyHandler, "shift_K": keyHandler, "shift_L": keyHandler,
-"shift_M": keyHandler, "shift_N": keyHandler, "shift_O": keyHandler, 
-"shift_P": keyHandler, "shift_Q": keyHandler, "shift_R": keyHandler,
-"shift_S": keyHandler, "shift_T": keyHandler, "shift_U": keyHandler,
-"shift_V": keyHandler, "shift_W": keyHandler, "shift_X": keyHandler,
-"shift_Y": keyHandler, "shift_Z": keyHandler, 
-
-"0": keyHandler, "1": keyHandler, "2": keyHandler, "3": keyHandler,
-"4": keyHandler, "5": keyHandler, "6": keyHandler, "7": keyHandler,
-"8": keyHandler, "9": keyHandler, "-": keyHandler, "=": keyHandler,
-"[": keyHandler, "]": keyHandler, "\\": keyHandler, ";": keyHandler,
-"'": keyHandler, ",": keyHandler, ".": keyHandler, "/": keyHandler,
-"`": keyHandler,
-
-// Cap lock on
-")": keyHandler, "!": keyHandler, "@": keyHandler, "#": keyHandler,
-"$": keyHandler, "%": keyHandler, "^": keyHandler, "&": keyHandler,
-"*": keyHandler, "(": keyHandler, "_": keyHandler, "+": keyHandler,
-"{": keyHandler, "}": keyHandler, "|": keyHandler, ":": keyHandler,
-"\"": keyHandler, "<": keyHandler, ">": keyHandler, "?": keyHandler,
-"~": keyHandler,"`": keyHandler,
-
-
-"shift_)": keyHandler, "shift_!": keyHandler, "shift_@": keyHandler,
-"shift_#": keyHandler, "shift_$": keyHandler, "shift_%": keyHandler,
-"shift_^": keyHandler, "shift_&": keyHandler, "shift_*": keyHandler,
-"shift_(": keyHandler, "shift__": keyHandler, "shift_+": keyHandler,
-"shift_{": keyHandler, "shift_}": keyHandler, "shift_|": keyHandler,
-"shift_:": keyHandler, "shift_\"": keyHandler, "shift_<": keyHandler,
-"shift_>": keyHandler, "shift_?": keyHandler, "shift_~": keyHandler,
-
-
-
-"Backspace": keyHandler, "spacebar": keyHandler, ".":keyHandler,
-"Enter":keyHandler, "Tab": keyHandler,
-
-// TEST  HERE
- // "shift_I": keyHandler,
-
-
-
-};  // End myBindings
-
-var myKeyMap = new Keymap(myBindings);
-
+// Install keymap on document
 myKeyMap.install(document);
 
 if (debug) {
---
+++ public/js/keyBindings.js
@@ -0,0 +1,68 @@
+// keyBindings.js
+
+'use strict';
+
+var debug = true;
+if (debug) console.log('keyBindings.js');
+
+// We pass back an object of key:value pairs
+// to install in the Keymap class
+function keyHandlerBingings() {  
+
+    return {
+        "a": keyHandler, "b": keyHandler, "c": keyHandler, "d": keyHandler,
+        "e": keyHandler, "f": keyHandler, "g": keyHandler, "h": keyHandler,
+        "i": keyHandler, "j": keyHandler, "k": keyHandler, "l": keyHandler,
+        "m": keyHandler, "n": keyHandler, "o": keyHandler, "p": keyHandler,
+        "q": keyHandler, "r": keyHandler, "s": keyHandler, "t": keyHandler,
+        "u": keyHandler, "v": keyHandler, "w": keyHandler, "x": keyHandler,
+        "y": keyHandler, "z": keyHandler, 
+
+        // Cap lock held down
+        "A": keyHandler, "B": keyHandler, "C": keyHandler, "D": keyHandler,
+        "E": keyHandler, "F": keyHandler, "G": keyHandler, "H": keyHandler,
+        "I": keyHandler, "J": keyHandler, "K": keyHandler, "L": keyHandler,
+        "M": keyHandler, "N": keyHandler, "O": keyHandler, "P": keyHandler,
+        "Q": keyHandler, "R": keyHandler, "S": keyHandler, "T": keyHandler,
+        "U": keyHandler, "V": keyHandler, "W": keyHandler, "X": keyHandler, 
+        "Y": keyHandler, "Z": keyHandler, 
+
+        "shift_A": keyHandler, "shift_B": keyHandler, "shift_C": keyHandler, 
+        "shift_D": keyHandler, "shift_E": keyHandler, "shift_F": keyHandler,
+        "shift_G": keyHandler, "shift_H": keyHandler, "shift_I": keyHandler,
+        "shift_J": keyHandler, "shift_K": keyHandler, "shift_L": keyHandler,
+        "shift_M": keyHandler, "shift_N": keyHandler, "shift_O": keyHandler, 
+        "shift_P": keyHandler, "shift_Q": keyHandler, "shift_R": keyHandler,
+        "shift_S": keyHandler, "shift_T": keyHandler, "shift_U": keyHandler,
+        "shift_V": keyHandler, "shift_W": keyHandler, "shift_X": keyHandler,
+        "shift_Y": keyHandler, "shift_Z": keyHandler, 
+
+        "0": keyHandler, "1": keyHandler, "2": keyHandler, "3": keyHandler,
+        "4": keyHandler, "5": keyHandler, "6": keyHandler, "7": keyHandler,
+        "8": keyHandler, "9": keyHandler, "-": keyHandler, "=": keyHandler,
+        "[": keyHandler, "]": keyHandler, "\\": keyHandler, ";": keyHandler,
+        "'": keyHandler, ",": keyHandler, ".": keyHandler, "/": keyHandler,
+        "`": keyHandler,
+
+        // Cap lock on
+        ")": keyHandler, "!": keyHandler, "@": keyHandler, "#": keyHandler,
+        "$": keyHandler, "%": keyHandler, "^": keyHandler, "&": keyHandler,
+        "*": keyHandler, "(": keyHandler, "_": keyHandler, "+": keyHandler,
+        "{": keyHandler, "}": keyHandler, "|": keyHandler, ":": keyHandler,
+        "\"": keyHandler, "<": keyHandler, ">": keyHandler, "?": keyHandler,
+        "~": keyHandler,"`": keyHandler,
+
+        "shift_)": keyHandler, "shift_!": keyHandler, "shift_@": keyHandler,
+        "shift_#": keyHandler, "shift_$": keyHandler, "shift_%": keyHandler,
+        "shift_^": keyHandler, "shift_&": keyHandler, "shift_*": keyHandler,
+        "shift_(": keyHandler, "shift__": keyHandler, "shift_+": keyHandler,
+        "shift_{": keyHandler, "shift_}": keyHandler, "shift_|": keyHandler,
+        "shift_:": keyHandler, "shift_\"": keyHandler, "shift_<": keyHandler,
+        "shift_>": keyHandler, "shift_?": keyHandler, "shift_~": keyHandler,
+
+        "Backspace": keyHandler, "spacebar": keyHandler, ".":keyHandler,
+        "Enter":keyHandler, "Tab": keyHandler,
+
+    };  // End keyBindings
+
+}
